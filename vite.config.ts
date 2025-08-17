428d9bd905e220d6368cf0841cdf7adbb913cc44
Liang Cian <trtiger4520@gmail.com>
2025-08-17 10:55:21 +0800

    chore: add devTools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,5 @@ export default defineConfig({
   server: {
     port: 4180,
   },
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), UnoCSS(), vueDevTools()],
 })
